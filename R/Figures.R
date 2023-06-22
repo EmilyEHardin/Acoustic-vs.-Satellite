41edae776e9974f608105b25c8381b341fccc10b
EmilyEHardin <hardinee22@gmail.com>
2023-06-21 23:26:42 -0400

    Updates and additional figures

--- R/Figures.R
+++ R/Figures.R
@@ -61,52 +61,54 @@ habitatmap <- ggplot() +
 ggsave(filename = "habitatmap.tiff", plot = habitatmap, device = "tiff", path = "/Users/EmilyHardin/Desktop/Research/Acous.vs.Sat/From Mariana", width = 240, height = 240, units = c("mm"), dpi = 400)
 
 
-## SSM SATELLITE TRACKS AND COAs MAPS ## 
+## CTMM, SSM, and COAs MAPS ## 
 
 # individual maps combined with outside software
-# this code uses the post-SSM satellite data {move} objects and COA locations {move} objects from "Individual dBBMMs" script. If these objects are not already loaded, please visit that script, or load in data from associated csv files. 
-
 library(sf)
 library(ggplot2)
 library(ggspatial)
 library(dplyr)
 library(move)
 
-# satellite track {move} objects needs to be converted to dataframes 
-track.As <- as.data.frame(As)
-track.Bs <- as.data.frame(Bs)
-track.Cs <- as.data.frame(Cs)
-track.Ds <- as.data.frame(Ds)
-track.Es <- as.data.frame(Es)
-track.Gs <- as.data.frame(Gs)
-track.Hs <- as.data.frame(Hs)
-track.Is <- as.data.frame(Is)
-
-# acoustic COA info needs to be converted to 
-Ea <- acoustic[["X64480"]] # Turtle E needs to be subset from overall {move} object, since it didn't have enough data for the dBBMMs 
-Ha <- acoustic[["X64483"]] # Turtle H needs to be subset from overall {move} object, since it didn't have enough data for the dBBMMs  
-det.Aa <- as.data.frame(Aa)
-det.Ca <- as.data.frame(Ca)
-det.Da <- as.data.frame(Da)
-det.Ea <- as.data.frame(Ea)
-det.Fa <- as.data.frame(Fa)
-det.Ga <- as.data.frame(Ga)
-det.Ha <- as.data.frame(Ha)
-det.Ia <- as.data.frame(Ia)
-
+# CTMM satellite transmissions = sat_ctmm
+sat_ctmm <- read.csv("sat_ctmm.csv", header = T)
+# SSM satellite transmissions = fG_noland
+sat_ssm <- read.csv("fG_noland.csv", header = T)
+# COAs = COA_all
+coas <- read.csv("COAall_loc.err.csv", header = T)
 # map code for all turtles except E and I (which have different coordinate system extents) + legend for COAs
-# change out IDs
-A_track <- ggplot() +
+
+# add geometry 
+sat_ctmm <- st_as_sf(sat_ctmm, coords = c("x", "y"), crs = 3395)
+sat_ctmm$lon<-st_coordinates(sat_ctmm)[,1]
+sat_ctmm$lat<-st_coordinates(sat_ctmm)[,2]
+sat_ctmm <- sat_ctmm %>%
+  rename(id = tag.local.identifier)
+
+sat_ssm <- st_as_sf(sat_ssm, coords = c("x", "y"), crs = 3395)
+sat_ssm$lon<-st_coordinates(sat_ssm)[,1]
+sat_ssm$lat<-st_coordinates(sat_ssm)[,2]
+sat_ssm <- sat_ssm %>%
+  filter(Location.Err < 3000)
+
+coas <- st_as_sf(coas, coords = c("Longitude.coa", "Latitude.coa"), crs = 4326)
+coas <- st_transform(coas, 3395)
+coas$lon<-st_coordinates(coas)[,1]
+coas$lat<-st_coordinates(coas)[,2]
+
+# Turtle C
+C_all <- ggplot() +
   geom_sf(data = land, fill = "grey", col = "grey") +
   geom_point(data = recdata2, aes(x = lon, y = lat), col = "grey60", size = 0.8) +
-  geom_path(data = track.As, aes(x = x, y = y, col = "grey28"), col = "grey28") +
-  geom_point(data = det.Aa, aes(x = coords.x1, y = coords.x2), col = "red", show.legend = T) + 
-  coord_sf(crs = st_crs(3395), xlim = c(-8810937.70, -8838767.56), ylim = c(2937305.74, 2958814.52)) + 
-  scale_x_continuous(breaks = c(-79.30, -79.20)) +
-  scale_y_continuous(breaks = c(25.70, 25.78)) +
-  annotation_scale(style = "ticks", location = "br", text_cex = 0.85, line_width = 1, height = unit(0.35, "cm"), text_pad = unit(0.15, "cm"), pad_x = unit(0.35, "cm"), pad_y = unit(0.35, "cm")) + 
+  geom_path(data = (sat_ctmm %>% filter(id == "X169269")), aes(x = lon, y = lat), col = "grey28") +
+  geom_point(data = (coas %>% filter(Tag.ID == "64477")), aes(x = lon, y = lat), col = "red2", size = 0.8, show.legend = T) +
+  geom_path(data = (sat_ssm %>% filter(id == "169269")), aes(x = lon, y = lat), col = "dodgerblue3") +
+  coord_sf(crs = st_crs(3395), xlim = c(-8837467, -8791537), ylim = c(2922185, 2967935)) + 
+  scale_x_continuous(breaks = c(-79.30, -79.10)) +
+  scale_y_continuous(breaks = c(25.55, 25.70, 25.85)) +
+  annotation_scale(style = "ticks", location = "br", text_cex = 0.80, line_width = 1, height = unit(0.30, "cm"), text_pad = unit(0.15, "cm"), pad_x = unit(0.35, "cm"), pad_y = unit(0.35, "cm")) + 
   theme_bw() +
-  theme(text = element_text(size = 15)) +
+  theme(text = element_text(size = 12)) +
   theme(axis.text.y = element_text(angle = 90, hjust = 0.5)) + 
   theme(axis.ticks.y = element_line(size = 0.5)) + 
   theme(axis.ticks.x = element_line(size = 0.5)) +
@@ -114,22 +116,25 @@ A_track <- ggplot() +
         axis.title.y=element_blank(),
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank()) + 
-  annotate("text", x = -8838767.56, y = 2958814.52, label = "Turtle A", size = 6.5, hjust = 0, vjust = 1) + 
+  ggplot2::annotate("text", x = -8837467, y =  2967935, label = "Turtle C", size = 6.5, hjust = 0, vjust = 1) + 
   scale_color_manual(values = c("red" = "red"), labels = c(red = "COAs"), name = NULL, guide = guide_legend(override.aes = list(linetype = c("blank"), shape = c(20), fill = c("red"), size = c(3.5))))
-ggsave(filename = "A_track.tiff", plot = A_track, device = "tiff",path = "/Users/EmilyHardin/Desktop/Research/Acous.vs.Sat", width = 120, height = 120, units = c("mm"), dpi = 600)
 
-# map code for Turtle E + satellite track legend 
-E_track <- ggplot() +
+ggsave(filename = "C_all.tiff", plot = C_all, device = "tiff",path = "/Users/EmilyHardin/Desktop/Research/Acous.vs.Sat", width = 120, height = 120, units = c("mm"), dpi = 600)
+
+
+# Turtle A, B, D, F, and G
+G_all <- ggplot() +
   geom_sf(data = land, fill = "grey", col = "grey") +
   geom_point(data = recdata2, aes(x = lon, y = lat), col = "grey60", size = 0.8) +
-  geom_path(data = track.Es, aes(x = x, y = y, col = "grey28"), col = "grey28", show.legend = T) +
-  geom_point(data = det.Ea, aes(x = coords.x1, y = coords.x2), col = "red") + 
-  coord_sf(crs = st_crs(3395), xlim = c(-8794239.77, -8877729.39), ylim = c(2913231.07, 2977796.37)) + 
-  scale_x_continuous(breaks = c(-79.6, -79.4, -79.2)) +
-  scale_y_continuous(breaks = c(25.6, 25.8)) +
-  annotation_scale(style = "ticks", location = "br", text_cex = 0.85, line_width = 1, height = unit(0.35, "cm"), text_pad = unit(0.15, "cm"), pad_x = unit(0.35, "cm"), pad_y = unit(0.35, "cm")) + 
+  geom_path(data = (sat_ctmm %>% filter(id == "X169268")), aes(x = lon, y = lat), col = "grey28") +
+  geom_path(data = (sat_ssm %>% filter(id == "169268")), aes(x = lon, y = lat), col = "dodgerblue") +
+  geom_point(data = (coas %>% filter(Tag.ID == "64482")), aes(x = lon, y = lat, col = "red2"), col = "red2", size = 0.8, show.legend = T) +
+  coord_sf(crs = st_crs(3395), xlim = c(-8837467, -8805037), ylim = c(2929945, 2962375)) + 
+  scale_x_continuous(breaks = c(-79.35, -79.25, -79.15)) +
+  scale_y_continuous(breaks = c(25.65, 25.75, 25.85)) +
+  annotation_scale(style = "ticks", location = "br", text_cex = 0.80, line_width = 1, height = unit(0.30, "cm"), text_pad = unit(0.15, "cm"), pad_x = unit(0.35, "cm"), pad_y = unit(0.35, "cm")) + 
   theme_bw() +
-  theme(text = element_text(size = 15)) +
+  theme(text = element_text(size = 12)) +
   theme(axis.text.y = element_text(angle = 90, hjust = 0.5)) + 
   theme(axis.ticks.y = element_line(size = 0.5)) + 
   theme(axis.ticks.x = element_line(size = 0.5)) +
@@ -137,22 +142,68 @@ E_track <- ggplot() +
         axis.title.y=element_blank(),
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank()) + 
-  annotate("text", x = -8877729.39, y = 2977796.37, label = "Turtle E", size = 6.5, hjust = 0, vjust = 1) + 
-  scale_color_manual(values = c("grey28" = "grey28"), labels = c(grey28 = "Satellite Track"), name = NULL,guide = guide_legend(override.aes = list(linetype = c(1))))
+  ggplot2::annotate("text", x = -8837467, y =  2962375, label = "Turtle G", size = 6.5, hjust = 0, vjust = 1) + 
+  scale_color_manual(values = c("red2" = "red2"), labels = c(red = "COAs"), name = NULL, guide = guide_legend(override.aes = list(linetype = c("blank"), shape = c(20), fill = c("red2"), size = c(3.5))))
+
+ggsave(filename = "G_all.tiff", plot = G_all, device = "tiff",path = "/Users/EmilyHardin/Desktop/Research/Acous.vs.Sat", width = 120, height = 120, units = c("mm"), dpi = 600)
 
 
-ggsave(filename = "E_track.tiff", plot = E_track, device = "tiff",path = "/Users/EmilyHardin/Desktop/Research/Acous.vs.Sat", width = 120, height = 120, units = c("mm"), dpi = 600)
-
-# map code for Turtle I + legend for grey receiver points 
-I_track <- ggplot() +
+# Turtle H and I 
+H_all <- ggplot() +
   geom_sf(data = land, fill = "grey", col = "grey") +
-  geom_point(data = recdata2, aes(x = lon, y = lat, col = "grey60"), col = "grey60", size = 0.8, show.legend = T) +
-  geom_path(data = track.Is, aes(x = x, y = y, col = "grey28"), col = "grey28") +
-  geom_point(data = det.Ia, aes(x = coords.x1, y = coords.x2), col = "red", show.legend = T) + 
-  coord_sf(crs = st_crs(3395), xlim = c(-8849899.51, -8794239.77), ylim = c(2912763.26, 2955739.88)) + 
-  scale_x_continuous(breaks = c(-79.4, -79.20)) +
-  scale_y_continuous(breaks = c(25.55, 25.75)) +
-  annotation_scale(style = "ticks", location = "br", text_cex = 0.85, line_width = 1, height = unit(0.35, "cm"), text_pad = unit(0.15, "cm"), pad_x = unit(0.35, "cm"), pad_y = unit(0.35, "cm")) + 
+  geom_point(data = recdata2, aes(x = lon, y = lat), col = "grey60", size = 0.8) +
+  geom_path(data = (sat_ctmm %>% filter(id == "X169274")), aes(x = lon, y = lat), col = "grey28") +
+  geom_path(data = (sat_ssm %>% filter(id == "169274")), aes(x = lon, y = lat), col = "dodgerblue") +
+  geom_point(data = (coas %>% filter(Tag.ID == "64483")), aes(x = lon, y = lat), col = "red2", size = 0.8, show.legend = T) +
+  coord_sf(crs = st_crs(3395), xlim = c(-8836467, -8770037), ylim = c(2905870, 2972300)) + 
+  scale_x_continuous(breaks = c(-79.30, -79.10, -78.90)) +
+  scale_y_continuous(breaks = c(25.45, 25.65, 25.85)) +
+  annotation_scale(style = "ticks", location = "br", text_cex = 0.80, line_width = 1, height = unit(0.30, "cm"), text_pad = unit(0.15, "cm"), pad_x = unit(0.35, "cm"), pad_y = unit(0.35, "cm")) + 
+  theme_bw() +
+  theme(text = element_text(size = 12)) +
+  theme(axis.text.y = element_text(angle = 90, hjust = 0.5)) + 
+  theme(axis.ticks.y = element_line(size = 0.5)) + 
+  theme(axis.ticks.x = element_line(size = 0.5)) +
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) + 
+  ggplot2::annotate("text", x = -8836467, y =  2972300, label = "Turtle H", size = 6.5, hjust = 0, vjust = 1) + 
+  scale_color_manual(values = c("grey60", "dodgerblue", "red2)", labels = c("CTMM", "SSM", "COAs"), name = NULL))
+
+
+ggsave(filename = "H_all.tiff", plot = H_all, device = "tiff",path = "/Users/EmilyHardin/Desktop/Research/Acous.vs.Sat", width = 120, height = 120, units = c("mm"), dpi = 600)
+
+
+# Turtle E 
+E_all <- ggplot() +
+  geom_sf(data = land, fill = "grey", col = "grey") +
+  geom_point(data = recdata2, aes(x = lon, y = lat), col = "grey60", size = 0.8) +
+  geom_path(data = (sat_ctmm %>% filter(id == "X169273")), aes(x = lon, y = lat), col = "grey28") +
+  geom_path(data = (sat_ssm %>% filter(id == "169273")), aes(x = lon, y = lat), col = "dodgerblue") +
+  geom_point(data = (coas %>% filter(Tag.ID == "64480")), aes(x = lon, y = lat), col = "red2", size = 0.8, show.legend = T) +
+  coord_sf(crs = st_crs(3395), xlim = c(-8892870, -8763266), ylim = c(2890395, 3020000)) + 
+  scale_x_continuous(breaks = c(-79.0, -79.4, -79.8)) +
+  scale_y_continuous(breaks = c(25.4, 25.7, 26.0, 26.3)) +
+  annotation_scale(style = "ticks", location = "br", text_cex = 0.80, line_width = 1, height = unit(0.30, "cm"), text_pad = unit(0.15, "cm"), pad_x = unit(0.35, "cm"), pad_y = unit(0.35, "cm")) + 
+  theme_bw() +
+  theme(text = element_text(size = 12)) +
+  theme(axis.text.y = element_text(angle = 90, hjust = 0.5)) + 
+  theme(axis.ticks.y = element_line(size = 0.5)) + 
+  theme(axis.ticks.x = element_line(size = 0.5)) +
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) + 
+  ggplot2::annotate("text", x = -8892870, y =  3020000, label = "Turtle E", size = 6.5, hjust = 0, vjust = 1) + 
+  scale_color_manual(values = c("grey60", "dodgerblue", "red2)", labels = c("CTMM", "SSM", "COAs"), name = NULL))
+
+ggsave(filename = "E_all.tiff", plot = E_all, device = "tiff",path = "/Users/EmilyHardin/Desktop/Research/Acous.vs.Sat", width = 120, height = 120, units = c("mm"), dpi = 600)
+
+# rec legend
+ggplot() +
+  geom_sf(data = land, fill = "grey", col = "grey") +
+  geom_point(data = recdata2, aes(x = lon, y = lat, col = "grey60"), size = 0.8, show.legend = T) +
   theme_bw() +
   theme(text = element_text(size = 15)) +
   theme(axis.text.y = element_text(angle = 90, hjust = 0.5)) + 
@@ -162,12 +213,51 @@ I_track <- ggplot() +
         axis.title.y=element_blank(),
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank()) + 
-  annotate("text", x = -8849899.51, y = 2955739.88, label = "Turtle I", size = 6.5, hjust = 0, vjust = 1) + 
   theme(legend.position = "bottom", 
         legend.text = element_text(size = 14), 
         legend.title = element_text(size = 14)) + 
-  scale_color_manual(values = c("grey60" = "grey60"), labels = c(grey60 = "Acoustic Receivers"), name = NULL, guide = guide_legend(override.aes = list(linetype = c("blank"), shape = c(20), fill = c("grey60"), size = c(3.5))))
+  scale_color_manual(values = "grey60", labels = c(grey60 = "Acoustic Receivers"), name = NULL, guide = guide_legend(override.aes = list(linetype = c("blank"), shape = c(20), fill = c("grey60"), size = c(3.5))))
 
-ggsave(filename = "I_track.tiff", plot = I_track, device = "tiff",path = "/Users/EmilyHardin/Desktop/Research/Acous.vs.Sat/From Mariana", width = 120, height = 120, units = c("mm"), dpi = 600)
+ggsave(filename = "I_track.tiff", plot = I_track, device = "tiff",path = "/Users/EmilyHardin/Desktop/Research/Acous.vs.Sat", width = 120, height = 120, units = c("mm"), dpi = 600)
 
+
+# CTMM legend
+ctmm_legend <- ggplot() +
+  geom_sf(data = land, fill = "grey", col = "grey") +
+  geom_path(data = test, aes(x = lon, y = lat, col = "dodgerblue"), size = 0.8, show.legend = T) +
+  theme_bw() +
+  theme(text = element_text(size = 15)) +
+  theme(axis.text.y = element_text(angle = 90, hjust = 0.5)) + 
+  theme(axis.ticks.y = element_line(size = 0.5)) + 
+  theme(axis.ticks.x = element_line(size = 0.5)) +
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) + 
+  theme(legend.position = "bottom", 
+        legend.text = element_text(size = 14), 
+        legend.title = element_text(size = 14)) + 
+  scale_color_manual(values = c("dodgerblue" = "dodgerblue"), labels = c(dodgerblue = "CTMM track"), name = NULL, guide = guide_legend(override.aes = list(linetype = c(1), size = c(10))))
+
+ggsave(filename = "ctmm_legend.tiff", plot = ctmm_legend, device = "tiff",path = "/Users/EmilyHardin/Desktop/Research/Acous.vs.Sat", width = 120, height = 120, units = c("mm"), dpi = 600)
+
+# SSM legend
+ssm <- ggplot() +
+  geom_sf(data = land, fill = "grey", col = "grey") +
+  geom_path(data = test, aes(x = lon, y = lat, col = "grey28"), size = 0.8, show.legend = T) +
+  theme_bw() +
+  theme(text = element_text(size = 15)) +
+  theme(axis.text.y = element_text(angle = 90, hjust = 0.5)) + 
+  theme(axis.ticks.y = element_line(size = 0.5)) + 
+  theme(axis.ticks.x = element_line(size = 0.5)) +
+  theme(axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) + 
+  theme(legend.position = "bottom", 
+        legend.text = element_text(size = 14), 
+        legend.title = element_text(size = 14)) + 
+  scale_color_manual(values = c("grey28" = "grey28"), labels = c(grey28 = "SSM track"), name = NULL, guide = guide_legend(override.aes = list(linetype = c(1), size = c(10))))
+
+ggsave(filename = "ssm_legend.tiff", plot = ssm, device = "tiff",path = "/Users/EmilyHardin/Desktop/Research/Acous.vs.Sat", width = 120, height = 120, units = c("mm"), dpi = 600)
 
